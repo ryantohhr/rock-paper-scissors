f42cd20f0a7c765254e0bf0caa79ad7557156abd
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-22 21:20:52 +0800

    Added pseudocode for whole project

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello, world")
+function getComputerChoice() {
+    // Get random number (Math.random())
+
+    // Assign rock, paper and scissors values to equal divisions of possible numbers (use switch)
+} // Returns "Rock", "Paper" or "Scissors" (Computer's randomised choice)
+
+function getHumanChoice() {
+    // Prompt user for input (use prompy)
+
+} // Returns "Rock", "Paper" or "Scissors" (User's choice)
+
+// Track scores with variables, starting at 0
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    // Make choices case-insensitive
+
+    // Print round winner (use console.log)
+
+    // Increment humanScore or computerScore depending on winner
+}
+
+function playGame() {
+    // Play round 5 times
+}
