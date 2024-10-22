935a0baf54d516121eed098383088e770bdb0be4
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-22 21:59:43 +0800

    Implemented playGame

--- script.js
+++ script.js
@@ -109,4 +109,8 @@ function playRound(humanChoice, computerChoice) {
 
 function playGame() {
     // Play round 5 times
+    for (let i = 0; i < 5; i++){
+        console.log(`Round ${i+1}`);
+        playRound(getHumanChoice(), getComputerChoice());
+    };
 }
