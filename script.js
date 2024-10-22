84c38e8d376a068b59cf427ebaf214b5279dc876
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-22 21:32:07 +0800

    Implemented getComputerChoice

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function getComputerChoice() {
     // Get random number (Math.random())
+    let randomNum = Math.random();
 
-    // Assign rock, paper and scissors values to equal divisions of possible numbers (use switch)
+    // Assign rock, paper and scissors values to equal divisions of possible numbers
+    if (randomNum >= 0 && randomNum < (1 / 3)) {
+        return "Rock";
+    }
+    else if (randomNum >= (1 / 3) && randomNum < (2 / 3)) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
 } // Returns "Rock", "Paper" or "Scissors" (Computer's randomised choice)
 
 function getHumanChoice() {
-    // Prompt user for input (use prompy)
+    // Prompt user for input (use prompt)
 
 } // Returns "Rock", "Paper" or "Scissors" (User's choice)
 
